aa6540ae2e8f2d83605b27990cc46f06dd242c0b
Ankit <ankitkumar151615@gmail.com>
2023-02-22 01:12:24 +0530

    Added home section

--- main.js
+++ main.js
@@ -15,3 +15,17 @@ document.querySelector("#searchBtn").onclick=()=>{
 document.querySelector("#closeSearch").onclick=()=>{
     document.querySelector("#searchForm").classList.remove("active");
 }
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 7500,
+      disableOnInteraction: false
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true
+    },
+    loop:true,
+  });
